8c3e60696f2b9efcdcadc475137b79c9d6f646bb
Daniel Schömer <daniel.schoemer@gmx.net>
2020-11-19 18:05:18 +0100

    Added action article#edit and related links to edit articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -29,6 +29,13 @@ class ArticlesController < ApplicationController
   end
 
   def update
+    self.article = Article.find(params[:id])
+
+    if article.update(article_params)
+      redirect_to(article)
+    else
+      render 'edit'
+    end
   end
 
   def destroy

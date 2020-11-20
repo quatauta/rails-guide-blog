3f603114ba72138f37fdea4a32e6ec0f50be2016
Daniel Schömer <daniel.schoemer@gmx.net>
2020-11-20 07:55:23 +0100

    Updated ruby formatting with standardrb --fix for my own code

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -24,7 +24,7 @@ class ArticlesController < ApplicationController
     if article.save
       redirect_to article
     else
-      render 'new'
+      render "new"
     end
   end
 
@@ -34,7 +34,7 @@ class ArticlesController < ApplicationController
     if article.update(article_params)
       redirect_to(article)
     else
-      render 'edit'
+      render "edit"
     end
   end
 

197d9e38934bdf3d9d1cb0c86f3efe187e5d51f8
Daniel Schömer <daniel.schoemer@gmx.net>
2020-12-22 20:04:46 +0100

    Added TailwindCSS to learn using a CSS framework
    I like to learn about TailwindCSS.
    
    I used https://rubyyagi.com/tailwind-css-on-rails-6-intro/#changing-tailwind-css-default-config
    as a guide to add TailwindCSS.
    
    TailwindCSS 2 requires PostCSS 8, but most tooling of a Ruby on Rail 6.1
    and Webpacker 5 does not yet support PostCSS 8. It is required to use
    the TailwindCSS PostCSS 7 compatibility build as described at
    https://tailwindcss.com/docs/installation#post-css-7-compatibility-build

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  future: {},
+  purge: [
+    './src/**/*.erb',
+    './src/**/*.haml',
+    './src/**/*.html',
+    './src/**/*.jsx',
+    './src/**/*.slim',
+    './src/**/*.vue',
+  ],
+  theme: {},
+  variants: {},
+  plugins: [],
+}
